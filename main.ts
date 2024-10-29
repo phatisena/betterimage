33406b5f15535eb4d9d0dc94e8824e9a72eb5a2e
Phatisena <naphatism@hotmail.com>
2024-10-29 09:46:28 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+namespace Image {
+
+    //%blockid=img_imgwidth
+    //%block="$img=screen_image_picker width"
+    //%group="better image"
+    export function ImgWidth(img: Image) {
+        return img.width
+    }
+
+    //%blockid=img_imgheight
+    //%block="$img=screen_image_picker height"
+    //%group="better image"
+    export function ImgHeight(img: Image) {
+        return img.height
+    }
+
+    //%blockid=img_recol
+    //%block="$img re image from $lacol to $lbcol"
+    //%lacol.shadow=colorindexpicker
+    //%lbcol.shadow=colorindexpicker
+    //%group="better image"
+    export function ReAndFill(img: Image, lacol: number[], lbcol: number[]) {
+        let colnv = 0
+        let coli = 0
+        let ucol = 0
+        let oimg = image.create(img.width, img.height)
+        for (let imx = 0; imx < img.width; imx++) {
+            for (let imy = 0; imy < img.height; imy++) {
+                ucol = img.getPixel(imx, imy)
+                coli = lacol.indexOf(ucol)
+                if ( coli >= 0) { colnv = lacol[coli] } else { colnv = ucol}
+                if (ucol > 0) {
+                    if (coli >= 0) {
+                        oimg.setPixel(imx, imy, lbcol[coli])
+                    }
+                }
+            }
+        }
+        return oimg
+    }
+}
