43243cc9aaa174779338c2ae4bd2ce1e971319ee
Phatisena <naphatism@hotmail.com>
2024-11-08 12:54:18 +0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -57,4 +57,58 @@ namespace images {
         }
         return oimg
     }
+
+    /**
+     * advance image overalap
+     * the another image
+     * with scanning image
+     * to check overlap
+     */
+    //%blockid=img_imgoverlap
+    //%block="Image $ImgI=screen_image_picker overlaping OtherImage $ImgO=screen_image_picker At OffsetX $Ix OffsetY $Iy And DirX $Dx DirY $Dy"
+    //%Dx.min=-1 Dx.max=1 Dx.defl=0
+    //%Dy.min=-1 Dy.max=1 Dy.defl=0
+    //%group="better image"
+    //%inlineInputMode=inline
+    //%weight=40
+    function ImgOverlapImg(ImgI: Image, ImgO: Image, Ix: number, Iy: number, Dx: number, Dy: number) {
+        if (Dy == 0) {
+            if (Dx > 0) {
+                for (let Nx = 0; Nx < ImgI.width; Nx++) {
+                    for (let Ny = 0; Ny < ImgI.height; Ny++) {
+                        if (ImgI.getPixel(Nx, Ny) != 0 && ImgI.getPixel(Ix + Nx, Iy + Ny) != 0) {
+                            return false
+                        }
+                    }
+                }
+            } else if (Dx < 0) {
+                for (let Nx = ImgI.width; Nx >= 0; Nx--) {
+                    for (let Ny = 0; Ny < ImgI.height; Ny++) {
+                        if (ImgI.getPixel(Nx, Ny) != 0 && ImgI.getPixel(Ix + Nx, Iy + Ny) != 0) {
+                            return false
+                        }
+                    }
+                }
+            }
+        } else if (Dx == 0) {
+            if (Dy > 0) {
+                for (let Ny = 0; Ny < ImgI.height; Ny++) {
+                    for (let Nx = 0; Nx < ImgI.width; Nx++) {
+                        if (ImgI.getPixel(Nx, Ny) != 0 && ImgI.getPixel(Ix + Nx, Iy + Ny) != 0) {
+                            return false
+                        }
+                    }
+                }
+            } else if (Dy < 0) {
+                for (let Ny = ImgI.height; Ny >= 0; Ny--) {
+                    for (let Nx = 0; Nx < ImgI.width; Nx++) {
+                        if (ImgI.getPixel(Nx, Ny) != 0 && ImgI.getPixel(Ix + Nx, Iy + Ny) != 0) {
+                            return false
+                        }
+                    }
+                }
+            }
+        }
+        return false
+    }
 }
